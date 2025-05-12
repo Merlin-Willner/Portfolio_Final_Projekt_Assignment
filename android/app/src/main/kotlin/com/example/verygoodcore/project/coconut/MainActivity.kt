8632713c9e6b0449531a2895aa1c36909463af4c
Merlin-Willner <merlinwillner.programming@gmail.com>
2025-05-12 17:42:12 +0200

    First Commit with the VeryGoodCLI Architecture

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/project/coconut/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.project_coconut
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:project_coconut/app/app.dart';
+import 'package:project_coconut/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
