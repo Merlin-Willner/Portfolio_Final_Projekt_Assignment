73af72dff3d3753a0f1b4f072351ec19a4b54ba8
Merlin-Willner <merlinwillner.programming@gmail.com>
2025-05-19 16:29:48 +0200

    Changed the APP ID and BUNDLE ID, initialized firebase, changed the Team on XCode to my paid DevAccount and added the app id on Firebase for IOS (this is why the GoogleService-Info file in the ios directory is new)

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 
 class AppBlocObserver extends BlocObserver {
@@ -21,6 +22,7 @@ class AppBlocObserver extends BlocObserver {
 }
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
@@ -28,6 +30,8 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   Bloc.observer = const AppBlocObserver();
 
   // Add cross-flavor configuration here
+  // 🔥 Firebase init here
+  await Firebase.initializeApp();
 
   runApp(await builder());
 }
